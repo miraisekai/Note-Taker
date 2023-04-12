9d8b1342ad08111969e83a144320528bb8bcb06e
miraisekai <116041477+miraisekai@users.noreply.github.com>
2023-04-12 13:54:22 -0500

    routes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const homeRoutes = require("./routes/homeroutes");
-
+const apiRoutes = require("./routes/apiRoutes");
 
 const app = express();
 
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // Custome middleware including routes
-
+app.use("/api", apiRoutes);
 app.use("/", homeRoutes);
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
